725248a5f111e2c5e7023acce573fccd4abec431
airvic <aireamhevictor@gmail.com>
2023-01-09 10:58:00 +0100

    first commit

--- app.js
+++ app.js
@@ -88,9 +88,9 @@ app.get('/endpoint',(req,res)=>{
     
 })
 
-  app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/public/index.html/'));
-  })
+  // app.get('*',(req,res)=>{
+  //   res.sendFile(path.join(__dirname+'/public/index.html/'));
+  // })
 
 app.post('/explore', async(req,res)=>{
   article = [];
